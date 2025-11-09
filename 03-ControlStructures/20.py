8ea3da9d7848bae11a2f40dddcdbff7783c1ee5b
Michal Gniadek <mongolow0072005@gmail.com>
2025-11-09 20:45:23 +0100

    binarny

---
+++ 03-ControlStructures/20.py
@@ -0,0 +1,13 @@
+number = int(input('Podaj liszbę w systemie dziesiętnym '))
+bin_rev = ''
+while number > 0:
+    reszta = number % 2
+    if reszta > 0:
+        bin_rev = bin_rev + '1'
+    else:
+        bin_rev = bin_rev + '0'
+    reszta = 0
+    number = number // 2
+
+
+print(bin_rev[::-1])
---
+++ 03-ControlStructures/5.6.py
@@ -0,0 +1,12 @@
+U = 0 
+N = 12
+suma = 0 
+while U < N:
+    U = U + 1  
+    if U%2 == 0:
+        suma = suma + U
+    else:
+        continue
+    
+
+print(suma)
